6ed023a42075eefc24e9e221d68fe90f6483c690
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-02-01 11:41:51 +0530

    Add files via upload

---
+++ variable1.js
@@ -0,0 +1,8 @@
+let nam="Saquib";
+console.log(nam);
+
+//variabless cannot be reserved keywords
+
+let firstName='Bin';
+let lastName="Halim"; //modern best practice
+console.log(nam+firstName+lastName);
